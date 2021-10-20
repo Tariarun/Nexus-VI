63c5514b9bf91c238ffc97f577d9158bd50eab10
Tariarun <zailvaix12@gmail.com>
2021-10-20 11:49:36 +0200

    UI

---
+++ Nexus VI/Assets/Scripts/GameplayManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayManager : MonoBehaviour
+{
+    public static GameplayManager instance;
+
+    public GameObject pauseMenu;
+    public static bool isPaused;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+}
--- Nexus VI/Assets/Scripts/UIManager.cs
+++ Nexus VI/Assets/Scripts/UIManager.cs
@@ -1,9 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    public void OnClick_Pause()
+    {
+        if (!GameplayManager.isPaused)
+        {
+            GameplayManager.instance.PauseGame();
+        }
+        else
+        {
+            GameplayManager.instance.ResumeGame();
+        }
+    }
+
+    public void OnClick_Scene0()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void OnClick_Scene1()
+    {
+        SceneManager.LoadScene(1);
+    }
+
     public void OnClick_Exit()
     {
         Application.Quit();
