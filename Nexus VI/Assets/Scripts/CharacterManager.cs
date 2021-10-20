8c19f453fef1afd8a6b348a1bc3b761a876d33d9
Eugeniov <bastien.coquelin@gmail.com>
2021-10-20 15:29:51 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ Nexus VI/Assets/Scripts/CharacterManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterManager : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float translationUpDown = Input.GetAxisRaw("Vertical") * speed * Time.deltaTime;
+
+
+        float translationLeftRight = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
+
+        transform.Translate(translationLeftRight, 0, translationUpDown);
+    }
+}
